0817a7dcf87f8f990206813c9c6d425b3dc561b8
Pranav Jay <pranavjaybusiness@gmail.com>
2025-03-17 17:36:21 -0500

    UserInfoResponse

--- Backend/src/main/java/com/middleman/authentication/payloads/response/UserInfoResponse.java
+++ Backend/src/main/java/com/middleman/authentication/payloads/response/UserInfoResponse.java
@@ -1,5 +1,37 @@
 package com.middleman.authentication.payloads.response;
 
+import java.util.List;
+
 public class UserInfoResponse {
+    private String id;
+    private String email;
+    private List<String> roles;
+
+    public UserInfoResponse(String id, String email, List<String> roles){
+        this.id = id;
+        this.email = email;
+        this.roles = roles;
+    }
+
+    public String getId() {
+        return id;
+      }
     
+      public void setId(String id) {
+        this.id = id;
+      }
+    
+      public String getEmail() {
+        return email;
+      }
+    
+      public void setEmail(String email) {
+        this.email = email;
+      }
+    
+      public List<String> getRoles() {
+        return roles;
+      }
+
+
 }
